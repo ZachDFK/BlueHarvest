73ffe53007293d1f29d678966a13554fc41586d7
unknown <matthewcrooke3@cb5109-40.labs.sce.carleton.ca>
2015-04-01 13:35:50 -0400

    voter

--- src/com/harvest/model/voter/Voters.java
+++ src/com/harvest/model/voter/Voters.java
@@ -1,11 +1,49 @@
 package com.harvest.model.voter;
 
-public class Voters implements Runnable {
+public class Voters {
+	private String name;
+	private int sinNumber; // ID
+	private int districtNum;
+	private String address;
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
+	public Voters(String name, int sinNumber, int districtNum, String address) {
+		this.name = name;
+		this.sinNumber = sinNumber;
+		this.districtNum = districtNum;
+		this.address = address;
+
 	}
-//	private PollingStation voteStation;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSinNumber() {
+		return sinNumber;
+	}
+
+	public void setSinNumber(int sinNumber) {
+		this.sinNumber = sinNumber;
+	}
+
+	public int getDistrictNum() {
+		return districtNum;
+	}
+
+	public void setDistrictNum(int districtNum) {
+		this.districtNum = districtNum;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 }
