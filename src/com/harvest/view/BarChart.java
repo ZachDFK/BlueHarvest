4eb97b0489d6fa5532ba02a2962d1dcd77b33113
matthewcrooke <matthewcrooke3@cmail.carleton.ca>
2015-04-08 15:35:00 -0400

    i think it works boss

--- src/com/harvest/view/BarChart.java
+++ src/com/harvest/view/BarChart.java
@@ -1,4 +1,5 @@
 package com.harvest.view;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -25,11 +26,8 @@ import javax.swing.SpringLayout;
 
 import com.harvest.shared.Constant;
 
-
-public class BarChart extends JPanel
-{
-	private Map<String, Integer> bars =
-            new LinkedHashMap<String, Integer>();
+public class BarChart extends JPanel {
+	private Map<String, Integer> bars = new LinkedHashMap<String, Integer>();
 	private DatagramSocket mediaViewToHeadSocket;
 	private int headServerPort;
 	private InetAddress headServerAddress;
@@ -38,42 +36,53 @@ public class BarChart extends JPanel
 	private ArrayList<JTextField> textFields;
 	private JScrollPane scrollBox;
 	private BarChart chart;
-	public void addBar(String name, int value)
-	{
+
+	public void addBar(String name, int value) {
 		bars.put(name, value);
 		repaint();
 	}
-	
-	public void establichConnectionWithHeadServer(){
+
+	public void establichConnectionWithHeadServer() {
 		Scanner input = new Scanner(System.in);
 		boolean successInput = false;
-	
+
 		try {
 			mediaViewToHeadSocket = new DatagramSocket();
-			
-			byte[] payload = Constant.HEAD_SERVER_MEDIA_REGISTRATION_CODE.getBytes();
+
+			byte[] payload = Constant.HEAD_SERVER_MEDIA_REGISTRATION_CODE
+					.getBytes();
 
 			DatagramPacket registrationPacket;
-			DatagramPacket registrationAcknowledgementPacket = new DatagramPacket(new byte[Constant.DATAGRAM_BUFFER_SIZE], Constant.DATAGRAM_BUFFER_SIZE);
-		
+			DatagramPacket registrationAcknowledgementPacket = new DatagramPacket(
+					new byte[Constant.DATAGRAM_BUFFER_SIZE],
+					Constant.DATAGRAM_BUFFER_SIZE);
+
 			String ip;
-			while(!successInput) {
+			while (!successInput) {
 				System.out.println("Enter the IP of the head server");
 				ip = input.nextLine();
-				
+
 				// Register polling station to district server
-				registrationPacket = new DatagramPacket(payload, payload.length,
-						InetAddress.getByName(ip), Constant.HEAD_SERVER_PORT);
+				registrationPacket = new DatagramPacket(payload,
+						payload.length, InetAddress.getByName(ip),
+						Constant.HEAD_SERVER_PORT);
 				mediaViewToHeadSocket.send(registrationPacket);
-	
+				System.out
+						.println("Sending media registration packet to head server.");
+
 				// Receive acknowledgment from district server
-				registrationAcknowledgementPacket = new DatagramPacket(new byte[Constant.DATAGRAM_BUFFER_SIZE], Constant.DATAGRAM_BUFFER_SIZE);
+				registrationAcknowledgementPacket = new DatagramPacket(
+						new byte[Constant.DATAGRAM_BUFFER_SIZE],
+						Constant.DATAGRAM_BUFFER_SIZE);
 				mediaViewToHeadSocket.setSoTimeout(Constant.DATAGRAM_TIMEOUT);
-				
+
 				try {
-					mediaViewToHeadSocket.receive(registrationAcknowledgementPacket);
+					mediaViewToHeadSocket
+							.receive(registrationAcknowledgementPacket);
+					System.out
+							.println("Connected media server to head server.");
 					successInput = true;
-				} catch(SocketTimeoutException e) {
+				} catch (SocketTimeoutException e) {
 					System.out.println("Could not connect to district server.");
 					successInput = false;
 				}
@@ -81,107 +90,98 @@ public class BarChart extends JPanel
 
 			headServerAddress = registrationAcknowledgementPacket.getAddress();
 			headServerPort = Constant.HEAD_SERVER_PORT;
-			
-			String message = new String(registrationAcknowledgementPacket.getData(), 0, registrationAcknowledgementPacket.getLength());
+
+			String message = new String(
+					registrationAcknowledgementPacket.getData(), 0,
+					registrationAcknowledgementPacket.getLength());
 
 			if (message.equals(Constant.SUCCESS_CONNECTION_ACK)) {
-				System.out.println("Media successfully connected to district server.");
+				System.out
+						.println("Media successfully connected to district server.");
 			} else {
 				System.out.println("Media did not connect to district server.");
 				mediaViewToHeadSocket.close();
 				return;
 			}
-		}catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Media cannot aquire port. Shutting down.");
 			return;
 		}
 		receivePacketing();
-			
-		
+
 	}
-	public void receivePacketing(){
-		
 
-		int blue = 1;
-		int green = 5;
-		int purple = 10;
-		while(true){
-//			byte[] recieveByte= new byte[Constant.DATAGRAM_BUFFER_SIZE];
-//			
-//			DatagramPacket updatedResults = new DatagramPacket(recieveByte, recieveByte.length);
-		
-//			try {
-//				mediaViewToHeadSocket.receive(updatedResults);
-				
-//				String resultString = new String(updatedResults.getData(),0,updatedResults.getLength());
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			
-				String resultString = "blue:"+ blue+ "%green:"+green +"%purple:"+purple+ "%";
-				purple++;
-				blue +=3;
-				green +=2;
-				
-				for(String c:resultString.split(Constant.CANDIDATES_STRING_DELIMITER)){
-					if(c.length() >0){
+	public void receivePacketing() {
+
+		//
+		// int blue = 1;
+		// int green = 5;
+		// int purple = 10;
+		while (true) {
+			byte[] recieveByte = new byte[Constant.DATAGRAM_BUFFER_SIZE];
+
+			DatagramPacket updatedResults = new DatagramPacket(recieveByte,
+					recieveByte.length);
+
+			try {
+				mediaViewToHeadSocket.receive(updatedResults);
+
+				String resultString = new String(updatedResults.getData(), 0,
+						updatedResults.getLength());
+
+				for (String c : resultString
+						.split(Constant.CANDIDATES_STRING_DELIMITER)) {
+					if (c.length() > 0) {
 						String[] params = c.split(Constant.DATA_DELIMITER);
-						//this.addBar(params[0],Integer.parseInt(params[1]));
 
-						
-						if(partiesNames.contains(params[0])){
-//							System.out.println("Exists");
-							textFields.get(partiesNames.indexOf(params[0])).setText(params[0] + Constant.DATA_DELIMITER + " " + params[1] );
-						}
-						else{
-//							System.out.println("New");
-							JTextField tempTextField = new JTextField(params[0] + Constant.DATA_DELIMITER + " " + params[1] );
-							
+						if (partiesNames.contains(params[0])) {
+							textFields.get(partiesNames.indexOf(params[0]))
+									.setText(
+											params[0] + Constant.DATA_DELIMITER
+													+ " " + params[1]);
+						} else {
+							JTextField tempTextField = new JTextField(params[0]
+									+ Constant.DATA_DELIMITER + " " + params[1]);
+
 							partiesNames.add(params[0]);
 							tempTextField.setEditable(false);
 							tempTextField.setVisible(true);
 							textFields.add(tempTextField);
-							
+
 							scores.add(tempTextField);
 						}
-						
+
 						scrollBox.setViewportView(scores);
-						chart.addBar(params[0],Integer.parseInt(params[1]));
+						chart.addBar(params[0], Integer.parseInt(params[1]));
 					}
 				}
-				
-				System.out.println("Received packet "+ resultString);
-				
-//			} catch( IOException e) {
-//				System.out.println("Could not connect to district server.");
-//			}
+
+				System.out.println("Received packet " + resultString);
+
+			} catch (IOException e) {
+				System.out.println("Could not connect to district server.");
+			}
 		}
-		
+
 	}
+
 	@Override
-	protected void paintComponent(Graphics g)
-	{
+	protected void paintComponent(Graphics g) {
 		// determine longest bar
-		
+
 		int max = Integer.MIN_VALUE;
-		for (Integer value : bars.values())
-		{
+		for (Integer value : bars.values()) {
 			max = Math.max(max, value);
 		}
-		
+
 		// paint bars
-		if(bars.size() > 0){
+		if (bars.size() > 0) {
 			int width = (getWidth() / bars.size()) - 2;
 			int x = 1;
-			for (String name : bars.keySet())
-			{
-				
+			for (String name : bars.keySet()) {
+
 				int value = bars.get(name);
-				int height = (int) 
-	                            ((getHeight()-5) * ((double)value / max));
+				int height = (int) ((getHeight() - 5) * ((double) value / max));
 				g.setColor(Color.getColor(name));
 				g.fillRect(x, getHeight() - height, width, height);
 				g.setColor(Color.getColor(name));
@@ -195,66 +195,38 @@ public class BarChart extends JPanel
 	public Dimension getPreferredSize() {
 		return new Dimension(bars.size() * 10 + 2, 50);
 	}
-	public void setUp(){
-//		int cand1Votes = 1;
-//		int cand2Votes = 3;
-//		int cand3Votes = 6;
-//		String party1 = "Liberal";
-//		String party2;
-//		JTextField cand1 = new JTextField(party1 + ":" + " " +Integer.toString(cand1Votes));
-//		JTextField cand2 = new JTextField(Integer.toString(cand2Votes));
-//		JTextField cand3 = new JTextField(Integer.toString(cand3Votes));
-//		cand1.setEditable(false);
-//		cand2.setEditable(false);
-//		cand3.setEditable(false);
+
+	public void setUp() {
 		JFrame frame = new JFrame("Bar Chart");
-		
-		scores = new JPanel(new GridLayout(0,1));
-		
-		
+
+		scores = new JPanel(new GridLayout(0, 1));
+
 		scrollBox = new JScrollPane(scores);
-		
-		
-		//scrollBox.setViewportView(scores);
+
 		scrollBox.setPreferredSize(new Dimension(10, 100));
-		
 
 		scrollBox.setVisible(true);
 		partiesNames = new ArrayList<String>();
 		textFields = new ArrayList<JTextField>();
-		
-		
-		
-		
+
 		frame.setLayout(new BorderLayout());
-		
-		
-		
+
 		chart = new BarChart();
-		
-//		chart.addBar(Color.red, cand1Votes);
-//		chart.addBar(Color.blue, cand2Votes);
-//		chart.addBar(Color.black, cand3Votes);
-//		chart.addBar("black",10);
-//		chart.addBar("blue",5);
-//	chart.addBar("red",2);
-	
 		frame.setResizable(true);
 		frame.add(scrollBox, BorderLayout.NORTH);
-		//frame.add(parties, BorderLayout.CENTER);
-		
+
 		frame.getContentPane().add((chart), BorderLayout.SOUTH);
-		
-		frame.setPreferredSize(new Dimension(250,350));
+
+		frame.setPreferredSize(new Dimension(250, 350));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
-		
+
 	}
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		BarChart nChart = new BarChart();
 		nChart.setUp();
-		nChart.receivePacketing();
+		nChart.establichConnectionWithHeadServer();
 	}
 }
